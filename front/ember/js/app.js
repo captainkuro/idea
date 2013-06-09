6904543f1e1b0de5c403e3ef1a8177a3266110e5
Captain kurO <me@captainkuro.com>
2013-06-09 11:19:11 +0700

    restructure

--- front/ember/js/app.js
+++ front/ember/js/app.js
@@ -1,11 +1,47 @@
-App = Ember.Application.create();
+Idea = Ember.Application.create();
 
-App.Router.map(function() {
+Idea.Router.map(function() {
   // put your routes here
+  this.route('login');
+  this.route('home');
+  this.route('logout');
 });
 
-App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
+Idea.IndexRoute = Ember.Route.extend({
+  redirect: function(router) {
+    if (Idea.currentUser.isLoggedIn) {
+      this.transitionTo('home');
+    } else {
+      this.transitionTo('login');
+    }
+  }
+});
+
+Idea.User = Ember.Object.extend({
+  username: '',
+  password: '',
+  name: '',
+
+  isLoggedIn: false
+});
+
+Idea.currentUser = Idea.User.create();
+
+Idea.LoginRoute = Ember.Route.extend({
+  user: Idea.currentUser,
+
+  enter: function() {
+    this.user.set('isLoggedIn', true);
+  }
+});
+
+Idea.HomeRoute = Ember.Route.extend({
+  user: Idea.currentUser
+});
+
+Idea.LogoutRoute = Ember.Route.extend({
+  enter: function() {
+    Idea.currentUser.set('isLoggedIn', false);
+    this.transitionTo('login');
   }
 });
